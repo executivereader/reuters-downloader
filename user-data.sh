b13f847b2b08fd92b8cec6a54796c3953b2783b0
executivereader <executivereader@users.noreply.github.com>
2015-07-27 16:41:22 -0400

    actually we do need to clone

--- user-data.sh
+++ user-data.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 sudo apt-get install -y git
 cd /home/ubuntu/
+sudo git clone https://github.com/executivereader/mongo-startup.git
 sudo git clone https://github.com/executivereader/reuters-downloader.git
 cd /home/ubuntu/reuters-downloader
-sudo wget https://raw.githubusercontent.com/executivereader/mongo-startup/master/connection_string.txt
+sudo cp /home/ubuntu/mongo-startup/connection_string.txt connection_string.txt
+sudo cp /home/ubuntu/mongo-startup/update_replica_set.py update_replica_set.py
 sudo python initialize_reuters_downloader.py
 sudo screen -dm -c "sudo python parse_reuters_articles.py"
