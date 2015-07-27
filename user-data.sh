25bde840bfc3dd3d2a5a740629f50bba2eaa1514
executivereader <executivereader@users.noreply.github.com>
2015-07-27 16:35:53 -0400

    wget connection string instead of git clone

--- user-data.sh
+++ user-data.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 sudo apt-get install -y git
 cd /home/ubuntu/
-sudo git clone https://github.com/executivereader/mongo-startup.git
 sudo git clone https://github.com/executivereader/reuters-downloader.git
 cd /home/ubuntu/reuters-downloader
-python initialize_reuters_downloader.py
-screen -dm -c "python parse_reuters_articles.py"
+sudo wget https://raw.githubusercontent.com/executivereader/mongo-startup/master/connection_string.txt
+sudo python initialize_reuters_downloader.py
+sudo screen -dm -c "sudo python parse_reuters_articles.py"
