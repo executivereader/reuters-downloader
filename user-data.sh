971e528ac21399c0d90bbb906120f5025faca059
executivereader <executivereader@users.noreply.github.com>
2015-07-27 17:05:38 -0400

    dependencies

--- user-data.sh
+++ user-data.sh
@@ -6,5 +6,7 @@ sudo git clone https://github.com/executivereader/reuters-downloader.git
 cd /home/ubuntu/reuters-downloader
 sudo cp /home/ubuntu/mongo-startup/connection_string.txt connection_string.txt
 sudo cp /home/ubuntu/mongo-startup/update_replica_set.py update_replica_set.py
+sudo apt-get install -y python-dev python-pip
+sudo pip install pymongo
 sudo python initialize_reuters_downloader.py
 sudo screen -dm -c "sudo python parse_reuters_articles.py"
