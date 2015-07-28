527a18d6d0d161b2ba561424a166fa18a0036dc7
executivereader <executivereader@users.noreply.github.com>
2015-07-28 14:21:51 -0400

    delete files once processed

--- parse_reuters_articles.py
+++ parse_reuters_articles.py
@@ -8,6 +8,9 @@ if __name__ == "__main__":
     client = start_mongo_client()
     for filename in os.listdir(path):
         if os.path.isfile(path + filename):
+            success = False
             with open(path + filename) as infile:
                 filedict = xmltodict.parse(infile)
-                client.tr.articles.insert(filedict)
+                success = client.tr.articles.insert(filedict)
+            if success:
+                os.remove(path + filename)
