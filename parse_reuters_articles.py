88b8bea2bb842969053f7067b2a1abc3a25978e7
executivereader <executivereader@users.noreply.github.com>
2015-07-28 13:39:02 -0400

    added code to inser TR articles

--- parse_reuters_articles.py
+++ parse_reuters_articles.py
@@ -1,1 +1,11 @@
 # code here
+import xmltodict
+import ox
+from update_replica_set import start_mongo_client
+
+if __name__ == "__main__":
+    client = start_mongo_client()
+    for filename in os.listdir("/home/ubuntu/output/Reuters World Service/"):
+        with open(filename) as infile:
+            filedict = xmltodict.parse(infile)
+            client.tr.articles.insert(filedict)
