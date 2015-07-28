347225ec99d27379e9a5311aeb859e713cbdc85c
executivereader <executivereader@users.noreply.github.com>
2015-07-28 14:12:47 -0400

    check if the operation is being done on a file

--- parse_reuters_articles.py
+++ parse_reuters_articles.py
@@ -4,8 +4,10 @@ import os
 from update_replica_set import start_mongo_client
 
 if __name__ == "__main__":
+    path = "/home/ubuntu/output/Reuters World Service/"
     client = start_mongo_client()
-    for filename in os.listdir("/home/ubuntu/output/Reuters World Service/"):
-        with open("/home/ubuntu/output/Reuters World Service/" + filename) as infile:
-            filedict = xmltodict.parse(infile)
-            client.tr.articles.insert(filedict)
+    for filename in os.listdir(path):
+        if os.path.isfile(path + filename):
+            with open(path + filename) as infile:
+                filedict = xmltodict.parse(infile)
+                client.tr.articles.insert(filedict)
