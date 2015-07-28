99f3ebf0e11be844376c3a10ba000687e65f335a
executivereader <executivereader@users.noreply.github.com>
2015-07-28 13:31:19 -0400

    add dependency

--- user-data.sh
+++ user-data.sh
@@ -8,6 +8,6 @@ cd /home/ubuntu/reuters-downloader
 sudo cp /home/ubuntu/mongo-startup/connection_string.txt connection_string.txt
 sudo cp /home/ubuntu/mongo-startup/update_replica_set.py update_replica_set.py
 sudo apt-get install -y python-dev python-pip
-sudo pip install pymongo
+sudo pip install pymongo xmltodict
 sudo python initialize_reuters_downloader.py
 sudo screen -dm -c "sudo python parse_reuters_articles.py"
