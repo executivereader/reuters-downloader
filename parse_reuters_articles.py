5828cf8f6a38919c7da45dc678878e0efb177688
executivereader <executivereader@users.noreply.github.com>
2015-07-28 13:41:42 -0400

    add path for files

--- parse_reuters_articles.py
+++ parse_reuters_articles.py
@@ -6,6 +6,6 @@ from update_replica_set import start_mongo_client
 if __name__ == "__main__":
     client = start_mongo_client()
     for filename in os.listdir("/home/ubuntu/output/Reuters World Service/"):
-        with open(filename) as infile:
+        with open("/home/ubuntu/output/Reuters World Service/" + filename) as infile:
             filedict = xmltodict.parse(infile)
             client.tr.articles.insert(filedict)
