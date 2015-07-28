b444665a01a1b8643d89ed9e1245383e699067b3
executivereader <executivereader@users.noreply.github.com>
2015-07-28 14:33:08 -0400

    error checking

--- parse_reuters_articles.py
+++ parse_reuters_articles.py
@@ -3,9 +3,15 @@ import xmltodict
 import os
 from update_replica_set import start_mongo_client
 
-if __name__ == "__main__":
-    path = "/home/ubuntu/output/Reuters World Service/"
-    client = start_mongo_client()
+def upload_all_documents_to_mongo(path, client):
+    """
+    Uploads all documents in path to Mongo.
+    Inputs:
+        path: a string with the path in which to find the XML documents
+        client: a pymongo MongoClient (preferably returned by update_replica_set.start_mongo_client())
+    Returns:
+        None
+    """
     for filename in os.listdir(path):
         if os.path.isfile(path + filename):
             success = False
@@ -14,3 +20,12 @@ if __name__ == "__main__":
                 success = client.tr.articles.insert(filedict)
             if success:
                 os.remove(path + filename)
+
+if __name__ == "__main__":
+    path = "/home/ubuntu/output/Reuters World Service/"
+    client = start_mongo_client()
+    while 1:
+        try:
+            upload_all_document_to_mongo(path, client)
+        except Exception as exc:
+            print exc
